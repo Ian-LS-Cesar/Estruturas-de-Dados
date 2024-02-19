9e5fbda9d7a584b823e9a6d9b6cef0ff64b38595
Ian-LS-Cesar <129915834+Ian-LS-Cesar@users.noreply.github.com>
2024-02-19 09:44:07 -0300

    Git init

---
+++ Revisao/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        byte byteVariable = 127; // 1 byte
+        System.out.println(byteVariable);
+        short shortVariable; // 2 bytes
+        int intVariable; // 4 bytes
+        long longVariable = 9_223_372_036_854_775_807L; // 8 bytes
+
+        float floatVariable = 0.85f; // 4 bytes
+        double doubleVariable = 0.672; // 8 bytes
+        
+        boolean booleanVariable = true;
+        char charVariable = 'L';
+    }
+}
