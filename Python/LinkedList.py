88bb39e94014fefb7cdbe33fcfa0ec6c89900d39
Ian Lucas S. César <iansantoscesar@gmail.com>
2024-04-25 00:20:01 -0300

    Python Linked List excluída

--- Python/LinkedList.py
+++ Python/LinkedList.py
@@ -1,121 +1,30 @@
+class No:
+    def __init(self, valor,comeco,fim):
+        self.valor = valor
+        self.comeco = None
+        self.fim = None
 
-class Node:
-    def __init__(self, data):
-        self.data = data 
-        self.next = None
+class ListaEncadeada:
 
-    def insertAtStart(self, data):
-        newNode = Node(data)
-        if self.head is None:
-            self.head = NewNode
-            return
+    def __init__(self):
+        self.comeco = None
+        self.fim = None
+        tamanho = 0
+
+    def inserirPrimeiro(self, valor):
+        novoNo = No(valor)
+        if tamanho == 0:
+            self.comeco = novoNo
+            self.fim = novoNo
         else:
-            newNode.next = self.head
-            self.head = newNode
-    
-    def insertByIndex(self, data, index):
-        newNode = Node(data)
-        currentNode = self.head
-        position = 0
+            novoNo.proximo = self.comeco
+            self.comeco = novoNo
+        tamanho += 1
 
-        if position == index:
-            self.insertStart(data)
-        else:
-            while(currentNode != None and (position + 1) != index ):
-                position+=1
-                currentNode = currentNode.next
+    def adicionar(self, valor):
+        novoNo = No(valor)
+        if tamanho == 0:
+            self.comeco = novoNo
+            self.fim = novoNo
 
-            if currentNode != None:
-                newNode.next = currentNode.next
-                currentNode.next = newNode
-            else:
-                raise("Index out of bounds!!!")
-    
-    def add(self, data):
-        newNode = Node(data)
-        if self.head is None:
-            self.head = newNode
-            return
-        
-        currentNode = self.head
-        while(currentNode.next):
-            currentNode = currentNode.next
-        
-        currentNode.next = newNode
 
-    def set(self, value, index):
-        currentNode = self.head
-        position = 0
-
-        if position == index:
-            currentNode.data = value
-        else:
-            while (currentNode != None and position != index):
-                position+=1
-                currentNode = currentNode.next
-            
-            if currentNode != None:
-                currentNode.data = value
-            else:
-                raise ("Index out of bounds!!!")
-    
-    def removeFirst(self):
-        if(self.head == None):
-            return
-
-        self.head = self.head.next
-
-    def removeLast(self):
-        if (self.head is None):
-            return
-
-        currentNode = self.head
-        while(currentNode.next.next):
-            currentNode = currentNode.next
-        
-        currentNode.next = None
-
-    def removeByIndex(self, index):
-        if self.head == None:
-            return
-        
-        currentNode = self.head
-        position = 0
-        if position == index:
-            self.removeFirst()
-        else:
-            while(currentNode != None and (position + 1) != index):
-                position+=1
-                currentNode = currentNode.next
-
-            if currentNode != None:
-                currentNode.next = currentNode.next.next
-            else:
-                raise("Index out of Bounds")
-    
-    def removeNode(self, data):
-        currentNode = self.head 
-
-        if currentNode.data == data:
-            self.removeFirst
-            return
-        
-        while currentNode is not None and currentNode.next.data != data:
-            currentNode = currentNode.next
-        
-        if currentNode is None:
-            return 
-        else:
-            currentNode.next = currentNode.next.next
-            
-    def size(self):
-        size = 0
-        if (self.head):
-            currentNode = self.head
-            while(currentNode):
-                size+=1
-                currentNode = currentNode.next
-            return size
-        else:
-            return 0
-    
